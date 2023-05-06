8d01e07e7e5e6fbd31d8f2345c973fc2abe8dee5
Ilya Malashenko <melax08@yandex.ru>
2023-05-06 16:42:07 +0400

    Add Dockerfile and some infra configs for launch the project in docker

--- pet_alert/pet_alert/settings.py
+++ pet_alert/pet_alert/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from django.core.management.utils import get_random_secret_key
 
@@ -53,10 +54,20 @@ WSGI_APPLICATION = 'pet_alert.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
+    }
+}
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -84,8 +95,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# if DEBUG:
+#     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
