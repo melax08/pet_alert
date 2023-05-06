f70fdc816981fb655bae76ae4d30e9c6fd48d3bd
Ilya Malashenko <melax08@yandex.ru>
2023-05-06 18:41:40 +0400

    Update admin.py for found and lost

--- pet_alert/ads/admin.py
+++ pet_alert/ads/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 from .models import Lost, Found
 
 
-admin.site.register(Lost)
-admin.site.register(Found)
+class LostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'active', 'pet_name', 'address', 'name', 'email')
+    list_editable = ('active', )
+    search_fields = ('description', 'pet_name', 'address')
+    empty_value_display = '-пусто-'
+
+
+class FoundAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'active', 'condition', 'address', 'name', 'email')
+    list_editable = ('active',)
+    search_fields = ('description', 'address')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Lost, LostAdmin)
+admin.site.register(Found, FoundAdmin)
