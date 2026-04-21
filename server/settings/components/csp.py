1131283724f3ee9e297383e9d3d222292b0a2965
Ilya Malashenko <melax08@yandex.ru>
2026-04-22 00:41:41 +0300

    ci: update github actions workflow

--- server/settings/components/csp.py
+++ server/settings/components/csp.py
@@ -14,6 +14,7 @@ CSP_SCRIPT_SRC: tuple[str, ...] = (
     "'unsafe-inline'",
     "cdn.jsdelivr.net",
     "127.0.0.1:8001",
+    "ajax.googleapis.com",
 )
 CSP_IMG_SRC: tuple[str, ...] = ("'self'", "data:")
 CSP_FONT_SRC: tuple[str, ...] = ("'self'", "fonts.gstatic.com", "fonts.googleapis.com")
